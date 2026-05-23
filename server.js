a8fbfe2bac41b7a468c984907a7c9e77344cc93e
Brendan <brendanathoele@gmail.com>
2026-05-23 14:48:32 -0500

    Make vite build to route index endpoint to React

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
+const __dirname = import.meta.dirname;
+
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 
 function App() {
-  return
+  return <div>Google</div>
 }
 
 export default App
