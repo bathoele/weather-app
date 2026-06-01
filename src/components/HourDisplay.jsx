2a030be8bc0255b6960586291b891b7a2fb21af8
Brendan <brendanathoele@gmail.com>
2026-06-01 14:57:16 -0500

    Route data through to HourItem

--- src/components/HourDisplay.jsx
+++ src/components/HourDisplay.jsx
@@ -1,15 +1,13 @@
 import React from "react";
+import HourItem from "./HourItem";
 
 function HourDisplay({data}) {
   const hours = data.hourly;
-  console.log(hours);
   return (
     <ul>
       {Object.entries(hours.time).map((hour) => (
         // li will instead be a separate component
-        <li key={hour}>
-          {hour}
-        </li>
+        <HourItem data={hours.time} />
       ))}
     </ul>
   )
---
+++ src/components/HourItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function HourItem({data}) {
+  console.log(data);
+  return
+}
+
+export default HourItem;
