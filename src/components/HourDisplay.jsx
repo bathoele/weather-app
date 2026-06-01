bf19a453e27710d4a62f5659f14891b67f8fe758
Brendan <brendanathoele@gmail.com>
2026-06-01 15:18:05 -0500

    Display hours and temp as list items

--- src/components/HourDisplay.jsx
+++ src/components/HourDisplay.jsx
@@ -3,11 +3,12 @@ import HourItem from "./HourItem";
 
 function HourDisplay({data}) {
   const hours = data.hourly;
+  // console.log(data);
   return (
     <ul>
-      {Object.entries(hours.time).map((hour) => (
+      {Object.entries(hours.time).map((hour, index) => (
         // li will instead be a separate component
-        <HourItem data={hours.time} />
+        <HourItem key={index} index={index} data={hours} />
       ))}
     </ul>
   )
--- src/components/HourItem.jsx
+++ src/components/HourItem.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-function HourItem({data}) {
-  console.log(data);
-  return
+function HourItem({index, data}) {
+  const time = data.time;
+  const temp = data.temperature_2m;
+  console.log(time[index], temp[index]);
+  return (
+    <li>{time[index]} {temp[index]}</li>
+  )
 }
 
 export default HourItem;
