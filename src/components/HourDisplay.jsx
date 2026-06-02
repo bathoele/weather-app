1ad08ea09efc903818c56efc50ccf1291e7da451
Brendan <brendanathoele@gmail.com>
2026-06-02 12:19:04 -0500

    Convert iso dates to days of week

--- src/components/HourDisplay.jsx
+++ src/components/HourDisplay.jsx
@@ -2,13 +2,31 @@ import React from "react";
 import HourItem from "./HourItem";
 
 function HourDisplay({data}) {
+  const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
   const hours = data.hourly.time;
   const temps = data.hourly.temperature_2m;
-  const itemsArray = [];
+  const items = [];
+  const forecastDates = [];
+  
+  hours.forEach((hour) => {
+    forecastDates.push(hour.split("T")[0]);
+  });
+
+  const days = [...new Set(forecastDates)];
+  days = days.forEach((day) => {
+    const date = new Date(day);
+
+    return `${date.getDay()}, ${date}`;
+  })
+
+  console.log(days);
 
   // Add function to join related data as objects in an array. Map through array, sending each object to HourItem
   for (let i = 0; i < hours.length; i++) {
-    itemsArray.push({
+    items.push({
       id: i + 1,
       time: hours[i],
       temp: temps[i]
@@ -17,7 +35,8 @@ function HourDisplay({data}) {
 
   return (
     <ul>
-      {itemsArray.map((item) => (
+      <h3></h3>
+      {items.map((item) => (
         // li will instead be a separate component
         <HourItem key={item.id} data={item} />
       ))}
--- src/components/HourItem.jsx
+++ src/components/HourItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 function HourItem({data}) {
+  
   return (
     <li>{data.time} {data.temp}</li>
   )
