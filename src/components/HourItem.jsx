30e82673df2a54315ecf2941affe75760c1ea1ab
Brendan <brendanathoele@gmail.com>
2026-06-02 18:22:10 -0500

    Add item for HourItem layout

--- src/components/HourItem.jsx
+++ src/components/HourItem.jsx
@@ -6,6 +6,8 @@ function HourItem({data: d}) {
     hour = "12am";
   }
   return (
+    // nest two? uls within the li, make each piece of data an li
+    // the first ul shows the basic data, the second ul is the drop down data
     <li>{hour} {d.temp} {d.humidity} {d.code} {d.w_dir} {d.w_speed} {d.cloud} {d.uv} {d.precip_per} </li>
   )
 }
