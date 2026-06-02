3615d43a2315bc844db16b32bf7b8e6cc5c12cbc
Brendan <brendanathoele@gmail.com>
2026-06-02 15:58:25 -0500

    Convert 0am to 12am

--- src/components/HourItem.jsx
+++ src/components/HourItem.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 function HourItem({data: d}) {
-
+  let hour = d.hour > 12 ? (d.hour - 12) + "pm" : d.hour + "am";
+  if (hour == "0am") {
+    hour = "12am";
+  }
   return (
-    <li>{d.hour} {d.temp} {d.humidity} {d.code} {d.w_dir} {d.w_speed} {d.cloud} {d.uv} {d.precip_per} </li>
+    <li>{hour} {d.temp} {d.humidity} {d.code} {d.w_dir} {d.w_speed} {d.cloud} {d.uv} {d.precip_per} </li>
   )
 }
 
