7ce6c82dd0ea9b5527440db51c7da38a967dc36f
Brendan <brendanathoele@gmail.com>
2026-06-03 09:03:53 -0500

    Add base HourItem dropdown ul

--- src/components/HourItem.jsx
+++ src/components/HourItem.jsx
@@ -8,7 +8,14 @@ function HourItem({data: d}) {
   return (
     // nest two? uls within the li, make each piece of data an li
     // the first ul shows the basic data, the second ul is the drop down data
-    <li>{hour} {d.temp} {d.humidity} {d.code} {d.w_dir} {d.w_speed} {d.cloud} {d.uv} {d.precip_per} </li>
+    <li>
+      <ul>
+        {hour} {d.code} {d.temp} {d.precip_per} {d.humidity}
+      </ul>
+      <ul>
+        {d.w_dir} {d.w_speed} {d.cloud} {d.uv}
+      </ul>
+    </li>
   )
 }
 
