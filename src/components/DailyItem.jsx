e343dc9c330f18ec56531aba092a07621218f6d3
Brendan <brendanathoele@gmail.com>
2026-06-04 08:02:01 -0500

    Populate DailyItem with data

--- src/components/DailyItem.jsx
+++ src/components/DailyItem.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 
 function DailyItem({data: d}) {
-   
-  return
+  console.log(d);
+  
+  return (
+    <ul key={d.id}>
+      <li>
+        {d.date}
+      </li>
+      <li>
+        {d.max_temp}
+      </li>
+      <li>
+        {d.min_temp}
+      </li>
+      <li>
+        {d.code}
+      </li>
+      <li>
+        {d.presip_per}
+      </li>
+      <li>
+        {d.precip_sum}
+      </li>
+      <li>
+        {d.w_dir}
+      </li>
+      <li>
+        {d.w_speed}
+      </li>
+      <li>
+        <ul>
+          <li>
+            {d.uv}
+          </li>
+          <li>
+            {d.humidity}
+          </li>
+        </ul>
+      </li>
+    </ul>
+  )
 }
 
 export default DailyItem;
