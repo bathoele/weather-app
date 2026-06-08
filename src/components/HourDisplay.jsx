b14e05cac37fd513cf0389ddeeb962940bf0c9d6
Brendan <brendanathoele@gmail.com>
2026-06-08 18:57:53 -0500

    Add accurate night time icons

--- src/components/HourDisplay.jsx
+++ src/components/HourDisplay.jsx
@@ -5,50 +5,50 @@ function HourDisplay({data}) {
   const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const nowHour = new Date().getHours();
-  const isDay = data.hourly.is_day === 1 ? "day" : "night";
 
   const icons = import.meta.glob('/node_modules/@meteocons/svg/monochrome/*.svg', {
     eager: true,
     query: '?url',
     import: 'default'
   })
-
-  const getIcon = (slug) => {
-    // add night param to track whether it is day or night, plug into url
-    return icons[`/node_modules/@meteocons/svg/monochrome/${slug}.svg`];
-  }
-
   // FIX THISS
+
   const weatherCodes = {
-    0:  { desc: "Clear Sky",                     icon: getIcon(`clear-${isDay}`) },
-    1:  { desc: "Mainly Clear",                  icon: getIcon(`mostly-clear-${isDay}`) },
-    2:  { desc: "Partly Cloudy",                 icon: getIcon(`partly-cloudy-${isDay}`) },
-    3:  { desc: "Overcast",                      icon: getIcon("overcast") },
-    45: { desc: "Fog",                           icon: getIcon(`fog-${isDay}`) },
-    48: { desc: "Depositing Rime Fog",           icon: getIcon(`fog-${isDay}`) },
-    51: { desc: "Light Drizzle",                 icon: getIcon("drizzle") },
-    53: { desc: "Moderate Drizzle",              icon: getIcon("drizzle") },
-    55: { desc: "Dense Drizzle",                 icon: getIcon("extreme-drizzle") },
-    56: { desc: "Light Freezing Drizzle",        icon: getIcon("sleet") },
-    57: { desc: "Heavy Freezing Drizzle",        icon: getIcon("sleet") },
-    61: { desc: "Slight Rain",                   icon: getIcon("rain") },
-    63: { desc: "Moderate Rain",                 icon: getIcon("rain") },
-    65: { desc: "Heavy Rain",                    icon: getIcon("extreme-rain") },
-    66: { desc: "Light Freezing Rain",           icon: getIcon("sleet") },
-    67: { desc: "Heavy Freezing Rain",           icon: getIcon("extreme-sleet") },
-    71: { desc: "Slight Snowfall",               icon: getIcon("snow") },
-    73: { desc: "Moderate Snowfall",             icon: getIcon("snow") },
-    75: { desc: "Heavy Snowfall",                icon: getIcon("extreme-snow") },
-    77: { desc: "Snow Grains",                   icon: getIcon("snow") },
-    80: { desc: "Slight Rain Showers",           icon: getIcon(`partly-cloudy-${isDay}-rain`) },
-    81: { desc: "Moderate Rain Showers",         icon: getIcon(`partly-cloudy-${isDay}-rain`) },
-    82: { desc: "Violent Rain Showers",          icon: getIcon("extreme-rain") },
-    85: { desc: "Slight Snow Showers",           icon: getIcon(`partly-cloudy-${isDay}-snow`) },
-    86: { desc: "Heavy Snow Showers",            icon: getIcon("extreme-snow") },
-    95: { desc: "Thunderstorm",                  icon: getIcon(`thunderstorms-${isDay}`) },
-    96: { desc: "Thunderstorm with Slight Hail", icon: getIcon(`thunderstorms-${isDay}-rain`) },
-    99: { desc: "Thunderstorm with Heavy Hail",  icon: getIcon(`thunderstorms-${isDay}-extreme-rain`) },
+    0:  { desc: "Clear Sky",                     icon: "clear-day" },
+    1:  { desc: "Mainly Clear",                  icon: "mostly-clear-day" },
+    2:  { desc: "Partly Cloudy",                 icon: "partly-cloudy-day" },
+    3:  { desc: "Overcast",                      icon: "overcast" },
+    45: { desc: "Fog",                           icon: "fog-day" },
+    48: { desc: "Depositing Rime Fog",           icon: "fog-day" },
+    51: { desc: "Light Drizzle",                 icon: "drizzle" },
+    53: { desc: "Moderate Drizzle",              icon: "drizzle" },
+    55: { desc: "Dense Drizzle",                 icon: "extreme-drizzle" },
+    56: { desc: "Light Freezing Drizzle",        icon: "sleet" },
+    57: { desc: "Heavy Freezing Drizzle",        icon: "sleet" },
+    61: { desc: "Slight Rain",                   icon: "rain" },
+    63: { desc: "Moderate Rain",                 icon: "rain" },
+    65: { desc: "Heavy Rain",                    icon: "extreme-rain" },
+    66: { desc: "Light Freezing Rain",           icon: "sleet" },
+    67: { desc: "Heavy Freezing Rain",           icon: "extreme-sleet" },
+    71: { desc: "Slight Snowfall",               icon: "snow" },
+    73: { desc: "Moderate Snowfall",             icon: "snow" },
+    75: { desc: "Heavy Snowfall",                icon: "extreme-snow" },
+    77: { desc: "Snow Grains",                   icon: "snow" },
+    80: { desc: "Slight Rain Showers",           icon: "partly-cloudy-day-rain" },
+    81: { desc: "Moderate Rain Showers",         icon: "partly-cloudy-day-rain" },
+    82: { desc: "Violent Rain Showers",          icon: "extreme-rain" },
+    85: { desc: "Slight Snow Showers",           icon: "partly-cloudy-day-snow" },
+    86: { desc: "Heavy Snow Showers",            icon: "extreme-snow" },
+    95: { desc: "Thunderstorm",                  icon: "thunderstorms-day" },
+    96: { desc: "Thunderstorm with Slight Hail", icon: "thunderstorms-day-rain" },
+    99: { desc: "Thunderstorm with Heavy Hail",  icon: "thunderstorms-day-extreme-rain" },
   };
+
+  const getIcon = (code, isDay) => {
+    const slug = weatherCodes[code].icon;
+    const name = isDay ? slug : slug.replace("day", "night");
+    return icons[`/node_modules/@meteocons/svg/monochrome/${name}.svg`];
+  }
   
   const h = data.hourly;
   const hours = h.time;
@@ -76,7 +76,7 @@ function HourDisplay({data}) {
       temp: h.temperature_2m[i],
       humidity: h.relative_humidity_2m[i],
       code: weatherCodes[h.weather_code[i]].desc,
-      icon: weatherCodes[h.weather_code[i]].icon,
+      icon: getIcon(h.weather_code[i], h.is_day[i]),
       w_dir: h.wind_direction_10m[i],
       w_speed: h.wind_speed_10m[i],
       cloud: h.cloud_cover[i],
