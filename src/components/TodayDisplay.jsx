07fe9c7d2cf5c4b8e1f6a89921ec82e705484686
Brendan <brendanathoele@gmail.com>
2026-06-12 09:45:04 -0500

    Add base grid format for today info

--- src/components/TodayDisplay.jsx
+++ src/components/TodayDisplay.jsx
@@ -7,26 +7,31 @@ function TodayDisplay({data, getIcon, weatherCodes, formatTime}) {
   const c = data.current;
 
   return (
-    <div>
-      <div>
-        <ul>
-          <li>{Math.floor(c.temperature_2m)}&deg;</li>
-          <li>{d.weather_code[0]}</li>
-          <li>{Math.floor(d.temperature_2m_max[0])}&deg;</li>
-          <li>{Math.floor(d.temperature_2m_min[0])}&deg;</li>
-        </ul>
+    <div className="grid grid-cols-5">
+      <div className="grid grid-cols-5 col-span-2">
+        <div className="col-span-2">
+          <p className="text-8xl">{Math.floor(c.temperature_2m)}&deg;</p>
+          <p className="text-3xl mb-2">{weatherCodes[d.weather_code[0]].desc}</p>
+          <span className="text-2xl">{Math.floor(d.temperature_2m_max[0])}&deg; / {Math.floor(d.temperature_2m_min[0])}&deg;</span>
+        </div>
+        <span className="col-span-3">
+          <img
+            src={getIcon(d.weather_code[0], true)}
+            alt={weatherCodes[d.weather_code[0]].desc}
+            className="w-1/1"
+          />
+        </span>
       </div>
-      <div>
-        <ul>
-          <li>{d.relative_humidity_2m_max[0]}</li>
-          <li>{d.uv_index_max[0]}</li>
-          <li>{d.wind_speed_10m_max[0]}mph</li>
-          <li>{d.wind_direction_10m_dominant[0]}</li>
-          <li>{d.precipitation_probability_max[0]}</li>
-          <li>{d.precipitation_sum[0]}</li>
-          <span>{formatTime(d.sunrise[0])}</span>
-          <span>{formatTime(d.sunset[0])}</span>
-        </ul>
+      <span></span>
+      <div className="col-span-2">
+        <span>{d.relative_humidity_2m_max[0]}</span>
+        <span>{d.uv_index_max[0]}</span>
+        <span>{d.wind_speed_10m_max[0]}mph</span>
+        <span>{d.wind_direction_10m_dominant[0]}</span>
+        <span>{d.precipitation_probability_max[0]}</span>
+        <span>{d.precipitation_sum[0]}</span>
+        <span>{formatTime(d.sunrise[0])}</span>
+        <span>{formatTime(d.sunset[0])}</span>
       </div>
       <TempChart data={data} getIcon={getIcon} weatherCodes={weatherCodes}/>
     </div>
